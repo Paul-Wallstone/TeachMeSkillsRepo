ac5a18704912577fc52e36d736c0d2f5775f43e2
Pavel Puchinski <pavel.puchinsky@gmail.com>
2021-03-01 18:40:20 +0300

    First commit

---
+++ src/HW1_2021_03_05/Task1_JaggedArray.java
@@ -0,0 +1,20 @@
+package HW1_2021_03_05;
+
+import java.util.Arrays;
+
+public class Task1_JaggedArray {
+    public static void main(String[] args) {
+        int firstSize = (int) (Math.random() * 20);
+        int secondSize = (int) (Math.random() * 20);
+        System.out.printf("Количество строк:%d \nКоличество столбцов:%d\n",firstSize,secondSize);
+        int[][] array = new int[firstSize][secondSize];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++)
+                array[i][j] = (int) (-100 + Math.random() * 100);
+        }
+        for (int i = 0; i < array.length; i++)
+            System.out.println(Arrays.toString(array[i]));
+        System.out.println();
+    }
+}
