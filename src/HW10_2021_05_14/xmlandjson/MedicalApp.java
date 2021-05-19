75aa26682828a7b6782872ee4d6b442f09929998
Pavel Puchinski <pavel.puchinsky@gmail.com>
2021-05-19 10:59:08 +0300

    HW10 done

--- src/HW10_2021_05_14/xmlandjson/MedicalApp.java
+++ src/HW10_2021_05_14/xmlandjson/MedicalApp.java
@@ -6,7 +6,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 
 public class MedicalApp {
-    public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
         System.out.println(MedicalSearch.findDoctor("Allison Cameron") + "\n");
         System.out.println(MedicalSearch.findDoctor("Robert Chase") + "\n");
         System.out.println(MedicalSearch.findDoctor("Braun Finch"));
